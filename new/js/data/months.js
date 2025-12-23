af6367ec4967c7a51bea4f965594f301ca4bf379
Salamatzuu <your-email@example.com>
2025-12-23 09:44:14 +0800

    Fix image paths for month pages

--- new/js/data/months.js
+++ new/js/data/months.js
@@ -5,13 +5,13 @@ export const months = [
     summary: " ",
     notes: " ",
     photos: [
-    { src: "./js/source/j3.jpg", alt: " ", caption: "First date of the year sa feliz" },
-    { src: "./js/source/j2.jpg", alt: " ", caption: "u and teeja pag uwi" },
-    { src: "./js/source/j4.jpg", alt: " ", caption: "u look so pretty" },
-    { src: "./js/source/j6.jpg", alt: " ", caption: " keeb date, natuto ikaw mag kutkot" },
-    { src: "./js/source/j5.jpg", alt: " ", caption: "gift for u" },
+    { src: "../js/source/j3.jpg", alt: " ", caption: "First date of the year sa feliz" },
+    { src: "../js/source/j2.jpg", alt: " ", caption: "u and teeja pag uwi" },
+    { src: "../js/source/j4.jpg", alt: " ", caption: "u look so pretty" },
+    { src: "../js/source/j6.jpg", alt: " ", caption: " keeb date, natuto ikaw mag kutkot" },
+    { src: "../js/source/j5.jpg", alt: " ", caption: "gift for u" },
     ],
-    thumbnail: "./js/source/j1.jpg"
+    thumbnail: "../js/source/j1.jpg"
   },
   {
     name: "February",
@@ -19,13 +19,13 @@ export const months = [
     summary: " ",
     notes: " ",
     photos: [
-        { src: "./js/source/f2.jpg", alt: " ", caption: "u look cute carrying ur flowers, me proud of the choice" },
-        { src: "./js/source/f3.jpg", alt: " ", caption: "me hapi u hapi hehehehe" },
-        { src: "./js/source/f4.jpg", alt: " ", caption: "me and dawg [parang di siya masaya (si dawg)]" },
-        { src: "./js/source/f1.jpg", alt: " ", caption: "u deserve more of these babe!" },
-        { src: "./js/source/f6.jpg", alt: " ", caption: "I'll forever hold you dearly, like this." },
+        { src: "../js/source/f2.jpg", alt: " ", caption: "u look cute carrying ur flowers, me proud of the choice" },
+        { src: "../js/source/f3.jpg", alt: " ", caption: "me hapi u hapi hehehehe" },
+        { src: "../js/source/f4.jpg", alt: " ", caption: "me and dawg [parang di siya masaya (si dawg)]" },
+        { src: "../js/source/f1.jpg", alt: " ", caption: "u deserve more of these babe!" },
+        { src: "../js/source/f6.jpg", alt: " ", caption: "I'll forever hold you dearly, like this." },
     ],
-    thumbnail: "./js/source/f5.jpg"
+    thumbnail: "../js/source/f5.jpg"
   },
   {
     name: "March",
@@ -33,12 +33,12 @@ export const months = [
     summary: " ",
     notes: " ",
     photos: [
-        { src: "./js/source/m2.jpg", alt: " ", caption: "na aya mag cdid XD" },
-        { src: "./js/source/m2.png", alt: " ", caption: "u look so pretty pt2,,, u glowing for real" },
-        { src: "./js/source/m3.jpg", alt: " ", caption: "Fun Fact: bday netong dino??? na to ay March 8 2025" },  
-        { src: "./js/source/m5.jpg", alt: " ", caption: "me and dawg(pero hapi dawg)" },
+        { src: "../js/source/m2.jpg", alt: " ", caption: "na aya mag cdid XD" },
+        { src: "../js/source/m2.png", alt: " ", caption: "u look so pretty pt2,,, u glowing for real" },
+        { src: "../js/source/m3.jpg", alt: " ", caption: "Fun Fact: bday netong dino??? na to ay March 8 2025" },  
+        { src: "../js/source/m5.jpg", alt: " ", caption: "me and dawg(pero hapi dawg)" },
     ],
-    thumbnail: "./js/source/m1.jpg"
+    thumbnail: "../js/source/m1.jpg"
   },
   {
     name: "April",
@@ -46,14 +46,14 @@ export const months = [
     summary: " ",
     notes: " ",
     photos: [
-        { src: "./js/source/a3.jpg", alt: " ", caption: "i miss the times na ako yung nanglilibre, bawi ako babe promise!" },
-        { src: "./js/source/a2.jpg", alt: " ", caption: "u look so pretty pt3 (ganda pic ko no!!)" },
-        { src: "./js/source/a4.jpg", alt: " ", caption: "u lead d way, me will follow (and steer)" },
-        { src: "./js/source/a5.jpg", alt: " ", caption: "sunog na dynamite" },
-        { src: "./js/source/a6.jpg", alt: " ", caption: "kia and u" },
-        { src: "./js/source/a7.jpg", alt: " ", caption: "kia and me (kia: cno b e2)" },
+        { src: "../js/source/a3.jpg", alt: " ", caption: "i miss the times na ako yung nanglilibre, bawi ako babe promise!" },
+        { src: "../js/source/a2.jpg", alt: " ", caption: "u look so pretty pt3 (ganda pic ko no!!)" },
+        { src: "../js/source/a4.jpg", alt: " ", caption: "u lead d way, me will follow (and steer)" },
+        { src: "../js/source/a5.jpg", alt: " ", caption: "sunog na dynamite" },
+        { src: "../js/source/a6.jpg", alt: " ", caption: "kia and u" },
+        { src: "../js/source/a7.jpg", alt: " ", caption: "kia and me (kia: cno b e2)" },
     ],
-    thumbnail: "./js/source/a1.png"
+    thumbnail: "../js/source/a1.png"
   },
   {
     name: "May",
@@ -61,13 +61,13 @@ export const months = [
     summary: " ",
     notes: " ",
     photos: [
-        { src: "./js/source/ma1.jpg", alt: " ", caption: "sarap luto mo and mo :3" },
-        { src: "./js/source/ma2.jpg", alt: " ", caption: "date agen, sa shang yayoi (not sure) ata to after mo driving school" },
-        { src: "./js/source/ma3.jpg", alt: " ", caption: "focused driver e, always fun to race w u" },  
-        { src: "./js/source/ma4.jpg", alt: " ", caption: "eto yung time na nag sosorry me sayo kasi u deserve better, gusto ko princess ka. ayaw ko nga pinapawisan ikaw babe, mahal na mahal ko ikaw!" },
+        { src: "../js/source/ma1.jpg", alt: " ", caption: "sarap luto mo and mo :3" },
+        { src: "../js/source/ma2.jpg", alt: " ", caption: "date agen, sa shang yayoi (not sure) ata to after mo driving school" },
+        { src: "../js/source/ma3.jpg", alt: " ", caption: "focused driver e, always fun to race w u" },  
+        { src: "../js/source/ma4.jpg", alt: " ", caption: "eto yung time na nag sosorry me sayo kasi u deserve better, gusto ko princess ka. ayaw ko nga pinapawisan ikaw babe, mahal na mahal ko ikaw!" },
         
       ],
-    thumbnail: "./js/source/ma1.png"
+    thumbnail: "../js/source/ma1.png"
   },
   {
     name: "June",
@@ -75,17 +75,17 @@ export const months = [
     summary: " ",
     notes: " ",
     photos: [
-        { src: "./js/source/ju2.jpg", alt: " ", caption: "pakiss nga" },
-        { src: "./js/source/ju3.jpg", alt: " ", caption: "ramen date sa double dragon!!! (biglaan after mo sa pitx)" },
-        { src: "./js/source/ju4.jpg", alt: " ", caption: "blep" }, 
-        { src: "./js/source/ju5.jpg", alt: " ", caption: "sungit mo (😒<-this u) "},
-        { src: "./js/source/ju6.jpg", alt: " ", caption: "u have pretty eyes and a smile that melts my heart 💅 🍔" },  
-        { src: "./js/source/ju7.jpg", alt: " ", caption: "focused driver e pt2" },  
-        { src: "./js/source/ju8.jpg", alt: " ", caption: "this was a fun night, naligaw ligaw pa tayo pauwi xD" },
-        { src: "./js/source/ju9.jpg", alt: " ", caption: "kabantay satin si dawg" },  
+        { src: "../js/source/ju2.jpg", alt: " ", caption: "pakiss nga" },
+        { src: "../js/source/ju3.jpg", alt: " ", caption: "ramen date sa double dragon!!! (biglaan after mo sa pitx)" },
+        { src: "../js/source/ju4.jpg", alt: " ", caption: "blep" }, 
+        { src: "../js/source/ju5.jpg", alt: " ", caption: "sungit mo (😒<-this u) "},
+        { src: "../js/source/ju6.jpg", alt: " ", caption: "u have pretty eyes and a smile that melts my heart 💅 🍔" },  
+        { src: "../js/source/ju7.jpg", alt: " ", caption: "focused driver e pt2" },  
+        { src: "../js/source/ju8.jpg", alt: " ", caption: "this was a fun night, naligaw ligaw pa tayo pauwi xD" },
+        { src: "../js/source/ju9.jpg", alt: " ", caption: "kabantay satin si dawg" },  
 
     ],
-    thumbnail: "./js/source/ju1.jpg"
+    thumbnail: "../js/source/ju1.jpg"
   },
   {
     name: "July",
@@ -93,11 +93,11 @@ export const months = [
     summary: " ",
     notes: " ",
     photos: [
-      { src: "./js/source/jl1.jpg", alt: " ", caption: "blep 2" },
-      { src: "./js/source/jl22.jpg", alt: " ", caption: "late pagpag w friends" },
-      { src: "./js/source/jl3.jpg", alt: " ", caption: "mine" },
+      { src: "../js/source/jl1.jpg", alt: " ", caption: "blep 2" },
+      { src: "../js/source/jl22.jpg", alt: " ", caption: "late pagpag w friends" },
+      { src: "../js/source/jl3.jpg", alt: " ", caption: "mine" },
     ],
-    thumbnail: "./js/source/jl1.png"
+    thumbnail: "../js/source/jl1.png"
   },
   {
     name: "August",
@@ -105,17 +105,17 @@ export const months = [
     summary: " ",
     notes: " ",
     photos: [
-      { src: "./js/source/au2.jpg", alt: " ", caption: "model? yes. masungit? no, comment :>" },
-      { src: "./js/source/au5.jpg", alt: " ", caption: "ang gulo!" },
-      { src: "./js/source/au6.jpg", alt: " ", caption: "kiss me, pls (nag pls ako)" },
-      { src: "./js/source/au7.jpg", alt: " ", caption: "ah yes. our boi " },
-      { src: "./js/source/au9.jpg", alt: " ", caption: "perfect hand placement" },
-      { src: "./js/source/au10.jpg", alt: " ", caption: "my passenger princess promax" },
-      { src: "./js/source/au11.jpg", alt: " ", caption: "walang nangyari sa bgc and sa buendia XD" },
-      { src: "./js/source/au12.jpg", alt: " ", caption: "I think the dog knows 😉" },
-      { src: "./js/source/au13.jpg", alt: " ", caption: "got it early babe! aug aug 23 palang!!!" },
+      { src: "../js/source/au2.jpg", alt: " ", caption: "model? yes. masungit? no, comment :>" },
+      { src: "../js/source/au5.jpg", alt: " ", caption: "ang gulo!" },
+      { src: "../js/source/au6.jpg", alt: " ", caption: "kiss me, pls (nag pls ako)" },
+      { src: "../js/source/au7.jpg", alt: " ", caption: "ah yes. our boi " },
+      { src: "../js/source/au9.jpg", alt: " ", caption: "perfect hand placement" },
+      { src: "../js/source/au10.jpg", alt: " ", caption: "my passenger princess promax" },
+      { src: "../js/source/au11.jpg", alt: " ", caption: "walang nangyari sa bgc and sa buendia XD" },
+      { src: "../js/source/au12.jpg", alt: " ", caption: "I think the dog knows 😉" },
+      { src: "../js/source/au13.jpg", alt: " ", caption: "got it early babe! aug aug 23 palang!!!" },
     ],
-    thumbnail: "./js/source/au8.jpg"
+    thumbnail: "../js/source/au8.jpg"
   },
   {
     name: "September",
@@ -123,15 +123,15 @@ export const months = [
     summary: " ",
     notes: " ",
     photos: [
-      { src: "./js/source/s2.jpg", alt: " ", caption: "my wife" },
-      { src: "./js/source/s3.jpg", alt: " ", caption: "look, my date is really pretty. anniv namin to and 21st bday niya !!!" },
-      { src: "./js/source/s4.jpg", alt: " ", caption: "super deserved!!" },
-      { src: "./js/source/s5.jpg", alt: " ", caption: "thanks for everything babe, ikaw kakampi ko talaga dyaan. sa lahat. mas lalo ko lang ikaw naging kakampi nung day na yan moving forward. you made me feel not alone, na may kasama ako palagi babe." },
-      { src: "./js/source/s6.jpg", alt: " ", caption: "back to regular programming, u studying and me sleeping on your bed" },
-      { src: "./js/source/s7.jpg", alt: " ", caption: "thanks sa libre, sarap neto babe!" },
-      { src: "./js/source/s8.jpg", alt: " ", caption: "hbd dawg" },
+      { src: "../js/source/s2.jpg", alt: " ", caption: "my wife" },
+      { src: "../js/source/s3.jpg", alt: " ", caption: "look, my date is really pretty. anniv namin to and 21st bday niya !!!" },
+      { src: "../js/source/s4.jpg", alt: " ", caption: "super deserved!!" },
+      { src: "../js/source/s5.jpg", alt: " ", caption: "thanks for everything babe, ikaw kakampi ko talaga dyaan. sa lahat. mas lalo ko lang ikaw naging kakampi nung day na yan moving forward. you made me feel not alone, na may kasama ako palagi babe." },
+      { src: "../js/source/s6.jpg", alt: " ", caption: "back to regular programming, u studying and me sleeping on your bed" },
+      { src: "../js/source/s7.jpg", alt: " ", caption: "thanks sa libre, sarap neto babe!" },
+      { src: "../js/source/s8.jpg", alt: " ", caption: "hbd dawg" },
     ],
-    thumbnail: "./js/source/s1.jpg"
+    thumbnail: "../js/source/s1.jpg"
   },
   {
     name: "October",
@@ -139,12 +139,12 @@ export const months = [
     summary: " ",
     notes: " ",
     photos: [
-      { src: "./js/source/o2.jpg", alt: " ", caption: "back to regular programming (while u stay pretty)" },
-      { src: "./js/source/o3.jpg", alt: " ", caption: "nagpic lang ng magkaharap tas magtatabi na tayo uli hehehehehhehe" },
-      { src: "./js/source/o4.jpg", alt: " ", caption: "kulang, wala ako sa chest mo na nakayakap ! aray ko bossing" },
-      { src: "./js/source/o5.jpg", alt: " ", caption: "biglaang antipolo, sori kung na stress ka ( • ̀ω•́ )" },
+      { src: "../js/source/o2.jpg", alt: " ", caption: "back to regular programming (while u stay pretty)" },
+      { src: "../js/source/o3.jpg", alt: " ", caption: "nagpic lang ng magkaharap tas magtatabi na tayo uli hehehehehhehe" },
+      { src: "../js/source/o4.jpg", alt: " ", caption: "kulang, wala ako sa chest mo na nakayakap ! aray ko bossing" },
+      { src: "../js/source/o5.jpg", alt: " ", caption: "biglaang antipolo, sori kung na stress ka ( • ̀ω•́ )" },
     ],
-    thumbnail: "./js/source/o1.jpg"
+    thumbnail: "../js/source/o1.jpg"
   },
   {
     name: "November",
@@ -152,11 +152,11 @@ export const months = [
     summary: " ",
     notes: " ",
     photos: [
-      { src: "./js/source/n1.png", alt: " ", caption: "early jog/walking. tas excited ka mag mcdo hehehehhe. sarap kain natin nun, and thanks sa libre babe. " },
-      { src: "./js/source/n2.jpg", alt: " ", caption: "MOMMY 🤪" },
-      { src: "./js/source/n3.png", alt: " ", caption: "wish u were always smiling like this" },
+      { src: "../js/source/n1.png", alt: " ", caption: "early jog/walking. tas excited ka mag mcdo hehehehhe. sarap kain natin nun, and thanks sa libre babe. " },
+      { src: "../js/source/n2.jpg", alt: " ", caption: "MOMMY 🤪" },
+      { src: "../js/source/n3.png", alt: " ", caption: "wish u were always smiling like this" },
     ],
-    thumbnail: "./js/source/n1.png"
+    thumbnail: "../js/source/n1.png"
   },
   {
     name: "December",
@@ -164,11 +164,11 @@ export const months = [
     summary: " ",
     notes: " ",
     photos: [
-      { src: "./js/source/d2.png", alt: " ", caption: "yes im looking at u babe, excuse my foot im sorry" },
-      { src: "./js/source/d3.png", alt: " ", caption: "i try na patagalin yung paglalakad natin ng ganto para mas matagal pa kitang kasama babe, kasi dyan pa lang namimiss ko na ikaw" },
-      { src: "./js/source/d5.png", alt: " ", caption: "tas dito malungkot na uli, i see u waving oh it hurts a bit lang na we have to separate for a while again and wala di pa rin ako sanay na di kita laging makikita! i miss u rn tuloy!!!!" },
-      { src: "./js/source/d4.png", alt: " ", caption: "u look so sexy babe, and yes bagay sayo yung new glasses mo, i love you!" },
+      { src: "../js/source/d2.png", alt: " ", caption: "yes im looking at u babe, excuse my foot im sorry" },
+      { src: "../js/source/d3.png", alt: " ", caption: "i try na patagalin yung paglalakad natin ng ganto para mas matagal pa kitang kasama babe, kasi dyan pa lang namimiss ko na ikaw" },
+      { src: "../js/source/d5.png", alt: " ", caption: "tas dito malungkot na uli, i see u waving oh it hurts a bit lang na we have to separate for a while again and wala di pa rin ako sanay na di kita laging makikita! i miss u rn tuloy!!!!" },
+      { src: "../js/source/d4.png", alt: " ", caption: "u look so sexy babe, and yes bagay sayo yung new glasses mo, i love you!" },
     ],
-    thumbnail: "./js/source/d1.png"
+    thumbnail: "../js/source/d1.png"
   }
 ];
